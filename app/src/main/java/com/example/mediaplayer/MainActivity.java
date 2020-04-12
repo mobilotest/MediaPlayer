9a7249bbfacefcc778e2f0361219bbf1a74ce293
mobilotest <mobilotest@gmail.com>
2020-04-12 11:27:03 -0700

    fixed layouts and added some elements into Virgins class

--- app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -17,19 +17,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-
-
         // create an array of words
-        final ArrayList<Name> words = new ArrayList<Name>();
-        words.add(new Name("father", "әpә", R.drawable.family_father, R.raw.family_father));
-        words.add(new Name("mother", "әṭa", R.drawable.family_mother, R.raw.family_mother));
-        words.add(new Name("son", "angsi", R.drawable.family_son, R.raw.family_son));
-        words.add(new Name("daughter", "tune", R.drawable.family_daughter, R.raw.family_daughter));
-        words.add(new Name("older brother", "taachi", R.drawable.family_older_brother, R.raw.family_older_brother));
-        words.add(new Name("younger brother", "chalitti", R.drawable.family_younger_brother, R.raw.family_younger_brother));
-        words.add(new Name("older sister", "teṭe", R.drawable.family_older_sister, R.raw.family_older_sister));
-        words.add(new Name("younger sister", "kolliti", R.drawable.family_younger_sister, R.raw.family_younger_sister));
+        final ArrayList<Proverb> names = new ArrayList<Proverb>();
+        names.add(new Proverb(R.string.header_3_parable, R.string.proverb_contractors, R.drawable.nayomniki);
+        names.add(new Proverb(R.string.header_3_parable, R.string.proverb_dig_talent, R.drawable.talent));
+        names.add(new Proverb(R.string.header_3_parable, R.string.good_samaritan, R.drawable.good));
+        names.add(new Proverb(R.string.header_3_parable, R.string.proverb_invited_chosen, R.drawable.pir));
+        names.add(new Proverb(R.string.header_3_parable, R.string.proverb_prodigal_son, R.drawable.prodigal));
+        names.add(new Proverb(R.string.header_3_parable, R.string.proverb_sower, R.drawable.sower));
+        names.add(new Proverb(R.string.header_3_parable, R.string.proverb_ten_virgins, R.drawable.virgins));
 
         WordAdapter adapter = new WordAdapter(this, words, R.color.category_family);
         ListView listView = (ListView) findViewById(R.id.list);
